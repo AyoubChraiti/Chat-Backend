2575f97c95af2867bda055e70c11b76625c32874
xraiti <xraiti@xraitis-MacBook-Pro.local>
2025-10-16 18:15:05 +0100

    ..

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,8 @@ export const config = {
     'http://localhost:3002',
     'http://127.0.0.1:3002',
     'http://localhost:5173',  // for vite
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://ayoubchraiti.github.io'  // GitHub Pages
   ],
   nodeEnv: process.env.NODE_ENV || 'development'
 };
